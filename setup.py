a771509c02027fa6ff8cecbe34dfbb9f5b4a3667
dominic.porter <dominic.porter@dbpsolutions.com>
2022-06-20 21:43:22 +0700

    20-06-2022-08:19

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ setup(
     packages=find_packages(),
     pymodules =['syncfail2ban', 'syncfail2ban-client', ],
     entry_points={
-        'console_scripts': ["syncfail2ban = syncfail2ban:main",
-                            "syncfail2ban-client = syncfail2ban-client:main"],
+        'console_scripts': ["syncfail2ban = syncfail2ban:main", ],
     },
     install_requires=['pyzmq~=22.3.0',
                       'requests~=2.25.1',
                       'configparser~=5.2.0'],
     url='',
     license='Apache License 2.0',
-    author='dominic porter',
-    author_email='dominic.porter@dbpsolutions.com',
+    author='Dominic Porter',
+    author_email='open.source@dbpsolutions.com',
     description='sync fail2ban jails across servers & with OPNSense aliases'
 )
