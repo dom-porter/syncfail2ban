01ad4b35e597ef7cb039c3a1a57ca6811b1590ba
dominic.porter <dominic.porter@dbpsolutions.com>
2022-06-20 09:55:02 +0100

    20-06-2022-12:39

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ setup(
     packages=find_packages(),
     package_dir={"": "src"},
     package_data={"src.data": ["config.cfg"]},
-    py_modules=["syncfail2ban", ],
+    py_modules=["syncfail2ban",
+                "AliasController",
+                "Firewall",
+                "OpnSense",
+                "SyncConfig",
+                "SyncOPNThread",
+                "UpdateThread", ],
     entry_points={
         'console_scripts': [
             'syncfail2ban = syncfail2ban:main'
