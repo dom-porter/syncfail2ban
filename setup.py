251f52107d85b2a261cf741eea8cd76b0837062f
dominic.porter <dominic.porter@dbpsolutions.com>
2022-06-20 10:20:18 +0100

    20-06-2022-12:39

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     entry_points={
         'console_scripts': [
             'syncfail2ban = syncfail2ban:main',
-            'syncfail2ban-client = syncfail2ban-client:main',
         ]
     },
     install_requires=['pyzmq~=22.3.0',
