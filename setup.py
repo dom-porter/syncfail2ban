a0c44080de01935dbef9d8a338cd4162ae8b4f0b
dominic.porter <dominic.porter@dbpsolutions.com>
2022-06-20 10:49:37 +0100

    20-06-2022-12:39

--- setup.py
+++ setup.py
@@ -3,20 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='syncfail2ban',
     version='0.0.1.19062022',
-    packages=find_packages(include=['src.syncfail2ban', 'src.syncfail2ban.*']),
+    packages=find_packages(),
     package_dir={"syncfail2ban": "src"},
-    package_data={"src.data": ["config.cfg"]},
-    py_modules=["syncfail2ban",
-                "AliasController",
-                "Firewall",
-                "OpnSense",
-                "SyncConfig",
-                "SyncOPNThread",
-                "UpdateThread", ],
     entry_points={
-        'console_scripts': [
-            'syncfail2ban = syncfail2ban:main',
-        ]
+        'console_scripts': ["syncfail2ban = syncfail2ban:main"],
     },
     install_requires=['pyzmq~=22.3.0',
                       'requests~=2.25.1',
