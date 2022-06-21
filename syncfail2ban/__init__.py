6bc425515042256c959d7be6d2fee5ca148befca
dominic.porter <dominic.porter@dbpsolutions.com>
2022-06-21 07:35:32 +0100

    21-06-2022-10:05

--- syncfail2ban/__init__.py
+++ syncfail2ban/__init__.py
@@ -13,7 +13,6 @@
 #  permissions and limitations under the License
 # *************************************************************************
 
-import pathlib
 import logging
 import time
 import zmq
